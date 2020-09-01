e54227f0d06a5587941580b317bac28276a9e39b
saeed lotfi <saeid0411@gmail.com>
2020-09-01 19:20:31 +0430

    unnecessary database deleted

--- app/src/main/java/com/bilgiland/movielist/di/AppModule.kt
+++ app/src/main/java/com/bilgiland/movielist/di/AppModule.kt
@@ -1,16 +1,11 @@
 package com.bilgiland.movielist.di
 
-import android.content.Context
-import androidx.room.Room
 import com.bilgiland.movielist.ConstValue.BASE_URL
-import com.bilgiland.movielist.ConstValue.DATABASE_NAME
-import com.bilgiland.movielist.data.db.MovieDatabase
 import com.bilgiland.movielist.data.remote.ApiService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
-import dagger.hilt.android.qualifiers.ApplicationContext
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -27,19 +22,5 @@ object AppModule {
         .baseUrl(BASE_URL)
         .build()
         .create(ApiService::class.java)
-
-
-    @Provides
-    @Singleton
-    fun getDataBase(@ApplicationContext context: Context) =
-        Room.databaseBuilder(
-            context,
-            MovieDatabase::class.java,
-            DATABASE_NAME
-        ).build()
-
-    @Provides
-    @Singleton
-    fun getMovieDao(db: MovieDatabase) = db.movieDao()
 
 }
